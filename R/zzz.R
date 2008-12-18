79afc4b42e5838ddfc5b8eb8f74bbefe7c76ec1b
Robert Castelo <robert.castelo@upf.edu>
2008-12-18 00:00:00 +0000

    version 0.3-2

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,12 @@
 # this is a stub package that points to its newer version deposited
 # in http://www.bioconductor.org
 
-.First.lib <- function(libname, pkgname) {
-  message(paste("\n  The package qp has been replaced by the package qpgraph",
-                "that forms part of the Bioconductor project and can be",
-                "downloaded from http://www.bioconductor.org as follows:\n",
-                "source(\"http://www.bioconductor.org/biocLite.R\")",
-                "biocLite(\"qpgraph\")",
-                "\n  Please do 'help(qp)' to find out more information",
-                "about transiting from qp to qpgraph", sep="\n  "))
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(paste("\n  The package qp has been replaced by the package qpgraph",
+                              "that forms part of the Bioconductor project and can be",
+                              "downloaded from http://www.bioconductor.org as follows:\n",
+                              "source(\"http://www.bioconductor.org/biocLite.R\")",
+                              "biocLite(\"qpgraph\")",
+                              "\n  Please do 'help(qp)' to find out more information",
+                              "about transiting from qp to qpgraph", sep="\n  "))
 }
